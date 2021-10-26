ac67058a6607f6d3f44c986d5da83301810508fa
ndatta <ndatta@ndatta01-PC>
2021-10-26 17:58:01 +0530

    Response Status

--- src/main/java/com/jobs/interviewexperienceapp/exceptions/CustomException.java
+++ src/main/java/com/jobs/interviewexperienceapp/exceptions/CustomException.java
@@ -3,6 +3,7 @@ package com.jobs.interviewexperienceapp.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ResponseStatus(value=HttpStatus.NOT_FOUND)
 public class CustomException extends RuntimeException{
 	/**
--- src/main/java/com/jobs/interviewexperienceapp/resources/InterviewExperienceResource.java
+++ src/main/java/com/jobs/interviewexperienceapp/resources/InterviewExperienceResource.java
@@ -2,6 +2,8 @@ package com.jobs.interviewexperienceapp.resources;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,8 +28,8 @@ public class InterviewExperienceResource {
 	}
 
 	@PostMapping
-	public InterviewExperience postInterviewExperience(@RequestBody InterviewExperience interviewExperience) {
-		return interviewExperienceService.saveInterviewExperience(interviewExperience);
+	public ResponseEntity<InterviewExperience> postInterviewExperience(@RequestBody InterviewExperience interviewExperience) {
+		return new ResponseEntity<InterviewExperience>(interviewExperienceService.saveInterviewExperience(interviewExperience), HttpStatus.CREATED);
 	}
 	
 	@GetMapping
@@ -36,20 +38,20 @@ public class InterviewExperienceResource {
 	}
 
 	@GetMapping("/{_id}")
-	public InterviewExperience getInterviewExperienceById(@PathVariable("_id") long _id) {
-		return interviewExperienceService.findInterviewExperience(_id);
+	public ResponseEntity<InterviewExperience> getInterviewExperienceById(@PathVariable("_id") long _id) {
+		return new ResponseEntity<InterviewExperience>(interviewExperienceService.findInterviewExperience(_id), HttpStatus.OK);
 	}
 
 	@PutMapping("/{_id}")
-	public InterviewExperience putInterviewExperience(@PathVariable("_id") long _id, @RequestBody InterviewExperience interviewExperience) {
-		return interviewExperienceService.updateInterviewExperience(_id, interviewExperience);
+	public ResponseEntity<InterviewExperience> putInterviewExperience(@PathVariable("_id") long _id, @RequestBody InterviewExperience interviewExperience) {
+		return new ResponseEntity<InterviewExperience>(interviewExperienceService.updateInterviewExperience(_id, interviewExperience), HttpStatus.CREATED);
 	}
 
 	@DeleteMapping("/{_id}")
-	public String deleteInterviewExperience(@PathVariable("_id") long _id) {
+	public ResponseEntity<String> deleteInterviewExperience(@PathVariable("_id") long _id) {
 		interviewExperienceService.deleteInterviewExperience(_id);
 		
-		return "Deleted Interview Experience with _id: " + _id; 
+		return new ResponseEntity<String>("Deleted Interview Experience with _id: " + _id, HttpStatus.OK); 
 	}
 	
 }
